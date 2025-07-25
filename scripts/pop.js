74dc645373fd21faa901fcb3cfd9c4c6cead72e1
WitnessMx <71900846+WitnessMx@users.noreply.github.com>
2025-07-25 14:16:42 -0600

    Update pop.js

--- scripts/pop.js
+++ scripts/pop.js
@@ -4,11 +4,69 @@ const closePopup = document.getElementById('closePopup');
 const Fecha_v = document.getElementById('fecha_cal');
 const html5QrCode = new Html5Qrcode(/* element id */ "qr-reader");
   document.getElementById('qr-boton').addEventListener('click',function(){
-  const html5QrCode = new Html5Qrcode("qr-reader");
-      const qrCodeSuccessCallback = (decodedText, decodedResult) => {
-          /* handle success */
-      };
-      const config = { fps: 10, qrbox: { width: 350, height: 350 } };
+const qrCodeSuccessCallback = (decodedText, decodedResult) => {
+    /* handle success */
+
+                    // do something when code is read
+                    //console.log(decodedText,decodedResult);
+                    var info=decodedText.split(" ");
+                    //console.log(info);
+                   // ID_red.value= info[0];
+                    var Fechas0= info[1].split("/");
+                   // console.log(Fechas0);
+
+                    const fecha_2=new Date(Fechas0[2],Fechas0[1],Fechas0[0]);
+        
+                    var dia=addZ(fecha_2.getDate());
+
+                    var mes=addZ(fecha_2.getMonth());
+                    var anio=fecha_2.getFullYear();
+                    //console.log(dia, mes,anio);
+                    Fecha_v.value=anio + "-" + mes + "-" + dia;
+                    var search1 =document.querySelector('input[class="search-input"]');
+                    search1.value=info[0];
+                    search1.focus();
+                    var opc;
+                    setTimeout(() => {
+                       opc=document.querySelectorAll('li[class="search-tip"]');
+                        //console.log(opc); 
+                        for (let i = 0; i < opc.length; i++) {
+                            //console.log(opc[i].innerHTML);
+                            if (search1.value==opc[i].innerHTML) {
+                                opc[i].click();
+                            }    
+                        }
+                        
+                    }, "1000");
+                    
+                    let ico=document.querySelector('div[title='+search1.value+']')
+                    console.log(ico);
+                    ico.click();
+                    IdShowV2(info[0]);
+                    closePopupFunc();
+                   
+                    
+                    html5QrCode.stop().then((ignore) => {
+                    // QR Code scanning is stopped.
+                    }).catch((err) => {
+                        // Stop failed, handle it.
+                        console.log(err);
+                    });
+                    
+     
+                var stop=document.getElementById("closePopup");
+                stop.addEventListener("click",function(){
+                    html5QrCode.stop().then((ignore) => {
+                    // QR Code scanning is stopped.
+                    }).catch((err) => {
+                    // Stop failed, handle it.
+                    });
+                })
+
+   
+    
+};
+const config = { fps: 10, qrbox: {  width: 350, height: 350} };
       
       // If you want to prefer back camera
       html5QrCode.start({ facingMode: "environment" }, config, qrCodeSuccessCallback);
